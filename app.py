32f59f26f3fb13cec50a48b97d07a46fedb54e7e
donghyun1500 <85468215+donghyun1500@users.noreply.github.com>
2021-06-08 20:59:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -27,5 +27,27 @@ def listing():
 #
 #     return jsonify({'msg':'코멘트가 저장되었습니다.'})
 
+@app.route('/sign_up/check_dup', methods=['POST'])    # 아이디 중복 확인.
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+@app.route('/sign_up/save', methods=['POST'])         #회원가입.
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
