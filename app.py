2943d6f35e71313e9925f19a1d2131d27be4cd9f
dorightk <77604219+dorightk@users.noreply.github.com>
2021-06-08 19:13:56 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbproject
 
 ## HTML을 주는 부분
 @app.route('/')
@@ -15,15 +15,17 @@ def home():
 
 @app.route('/memo', methods=['GET'])
 def listing():
-    parks = list(db.parks.find({}, {'_id' : False}))
-    return jsonify({'park_info':parks})
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg':'GET 연결되었습니다!'})
 
-## API 역할을 하는 부분
 # @app.route('/memo', methods=['POST'])
 # def saving():
-#     sample_receive = request.form['sample_give']
-#     print(sample_receive)
-#     return jsonify({'msg':'POST 연결되었습니다!'})
+#     comment_receive = request.form['comment_give']
+#     doc = {'comment': comment_receive}
+#     db.project.insert_one(doc)
+#
+#     return jsonify({'msg':'코멘트가 저장되었습니다.'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
